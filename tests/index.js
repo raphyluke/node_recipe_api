c1498e812fa1332a769ed49e6744f377a2e10dde
ledieuelephant <contact.raphaelenlive@gmail.com>
2023-06-19 08:22:39 +0200

    Setup cypress

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,5 @@
 /* 
-    Listing of all the tests to be run
+    Listing of all the tests to be run (Cypress, Mocha, Jest)
     1. Is the user able to login with the correct credentials?
     2. Is there an error when the user don't exist
     3. Is there an error when the password is wrong
