47c2276315f997c6184656622812f6a9956d31d4
ledieuelephant <contact.raphaelenlive@gmail.com>
2023-06-19 09:20:33 +0200

    Test cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,4 +2,7 @@ describe('Check if server is open and display without problem', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000')
   })
+  it('click on button', () => {
+    cy.get('.btn').click()
+  })
 })
