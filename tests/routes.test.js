b4fced77790f0e2a93e6eb869b6498846f789920
ledieuelephant <contact.raphaelenlive@gmail.com>
2023-06-24 04:18:39 +0200

    Test route POST with auth

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,27 +1,44 @@
 const supertest = require('supertest');
 const request = supertest(require('../api')());
+const jwt = require('jsonwebtoken');
 
-it("GET /api/recipes", async () => {
+var token = jwt.sign({ id: 1 }, process.env.JWT_PRIVATE_KEY, { expiresIn: 86400 });
+
+it("GET /api/recipes 200", async () => {
     const response = await request.get('/api/recipes');
     expect(response.status).toBe(200);
 })
 
-it("POST /api/recipes", async () => {
+it("POST /api/recipes 401", async () => {
     const response = await request.post('/api/recipes');
     expect(response.status).toBe(401);
 })
 
-it("GET /api/recipes/:id", async () => {
+it("GET /api/recipes/:id 401", async () => {
     const response = await request.get('/api/recipes/1');
     expect(response.status).toBe(401);
 })
 
-it("PUT /api/recipes/:id", async () => {
+it("PUT /api/recipes/:id 401", async () => {
     const response = await request.put('/api/recipes/1');
     expect(response.status).toBe(401);
 })
 
-it("DELETE /api/recipes/:id", async () => {
+it("DELETE /api/recipes/:id 401", async () => {
     const response = await request.delete('/api/recipes/1');
     expect(response.status).toBe(401);
 })
+
+it("POST /api/recipes 201", async () => {
+    const response = await request.post('/api/recipes')
+        .auth(token, { type: 'bearer' })
+        .send({
+            name: "Test Recipe",
+            type: "Test Type",
+            image: "Test Image",
+            prepatime : 10,
+            cooktime : 10,
+            resttime : 10,
+        })
+        expect(response.status).toBe(200);
+});
